f74f5d187b80cc8cbf0ae0dab8a77b3d55a9a521
josemespinoza <52440512+josemespinoza@users.noreply.github.com>
2020-10-26 12:55:55 +1100

    Update AnalysisQuery.sql
    Adding join between ticket table and EventAtendees using TicketId

--- AnalysisQuery.sql
+++ AnalysisQuery.sql
@@ -9,4 +9,4 @@ e.[EventName],
 t.[TicketType]
 from [Events].dbo.EventAtendees ea left join [Persons].dbo.Person p on p.Gender = ea.Gender and p.DateOfBirth = ea.DateOfBirth
 inner join [Events].dbo.Event e on e.Id = ea.EventId
-left join [Events].[dbo].[Ticket] t on t.[EventID] = e.Id;
+left join [Events].[dbo].[Ticket] t on t.Id = ea.[TicketID];
