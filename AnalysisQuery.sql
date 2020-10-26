b33e0852c24a11cdd1c66ae9447cd73568806ddc
josemespinoza <52440512+josemespinoza@users.noreply.github.com>
2020-10-26 12:33:17 +1100

    Create AnalysisQuery.sql

---
+++ AnalysisQuery.sql
@@ -0,0 +1,12 @@
+SELECT p.Id,
+p.Occupation,
+p.Gender,
+p.DateOfBirth,
+p.Suburb,
+p.State,
+p.PostCode,
+e.[EventName],
+t.[TicketType]
+from [Events].dbo.EventAtendees ea left join [Persons].dbo.Person p on p.Gender = ea.Gender and p.DateOfBirth = ea.DateOfBirth
+inner join [Events].dbo.Event e on e.Id = ea.EventId
+left join [Events].[dbo].[Ticket] t on t.[EventID] = e.Id;
